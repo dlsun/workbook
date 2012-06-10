8ff95e85703a98ee79271f7d905fc32c163dc471
Jonathan Taylor <jonathan.taylor@stanford.edu>
2012-06-10 15:35:45 -0700

    added homework files, with some extra json data in the .ipynb files

---
+++ homework_files/homework_builder/questions.py
@@ -0,0 +1,58 @@
+import numpy as np
+from IPython.core.displaypub import publish_display_data
+import json
+
+def multiple_choice(question_text, choices, correct_answer, identifier):
+    """
+    Generate a multiple choice question with given text and choices, and name the input form identifier
+    """
+    publish_display_data("HomeworkBuilder", {'text/latex':question_text})
+
+    # shuffle the choices
+    np.random.shuffle(choices)
+
+    radio_code = '\n'.join(['''
+    <p><input type="radio" name="%(name)s" value="%(value)s"> %(value)s</p>
+    ''' % {'name':identifier, 'value':choice} for choice in choices])
+
+    if correct_answer not in list(choices):
+        raise ValueError('the correct answer should be one of the choices!')
+
+    publish_display_data("HomeworkBuilder", {'text/html':radio_code})
+    publish_display_data("HomeworkBuilder", {'application/json': json.dumps({'question_identifier':identifier,
+                                                                             'checkable':True,
+                                                                             'correct_answer':correct_answer})})
+
+
+def text_box(question_text, correct_answer, identifier):
+    """
+    Generate a text box answer question with given textname the input form identifier
+    """
+    publish_display_data("HomeworkBuilder", {'text/latex':question_text})
+    textbox_code = '''
+    <p><input type="text" name="%(name)s"></p>
+    ''' % {'name':identifier} 
+
+    publish_display_data("HomeworkBuilder", {'text/html':textbox_code})
+    publish_display_data("HomeworkBuilder", {'application/json': json.dumps({'question_identifier':identifier,
+                                                                             'checkable':False,
+                                                                             'correct_answer':correct_answer})})
+
+def true_or_false(question_text, correct_answer, identifier):
+    """
+    Generate a true or false question with given textname the input form identifier
+    """
+    publish_display_data("HomeworkBuilder", {'text/latex':"(True/False) " + question_text})
+
+    choices = ['True', 'False']
+    radio_code = '\n'.join(['''
+    <p><input type="radio" name="%(name)s" value="%(value)s"> %(value)s</p>
+    ''' % {'name':identifier, 'value':choice} for choice in choices])
+
+    if correct_answer not in [True, False]:
+        raise ValueError('the correct answer should be one of [True, False]')
+    publish_display_data("HomeworkBuilder", {'text/html':radio_code})
+    publish_display_data("HomeworkBuilder", {'application/json': json.dumps({'question_identifier':identifier,
+                                                                 'checkable':True,
+                                                                             'correct_answer':correct_answer})})
+
